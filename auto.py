189b4bb983753c0a537711b387b746068cab4750
hyjoong <maplesosser@naver.com>
2022-01-20 23:50:47 +0900

    feat: 상단 메뉴 레이아웃 구현

--- auto.py
+++ auto.py
@@ -25,13 +25,48 @@ def type_input():
 def delete_list():
     listbox.delete(listbox.curselection()) # 선택한 항목 삭제
 
+def Radio_state():
+    print()
+
+labelTitle = Label(win,text="매크로")
+labelTitle.pack()
+
+menu = Menu(win)
+
+menu_file = Menu(menu, tearoff=0)
+menu_file.add_command(label="새 매크로")
+menu_file.add_command(label="매크로 저장")
+menu_file.add_command(label="매크로 불러오기")
+
+menu_start = Menu(menu, tearoff=0)
+menu_start.add_command(label="실행")
+menu_start.add_command(label="중지")
+
+menu_setting = Menu(menu, tearoff=0)
+menu_setting.add_command(label="키보드 설정")
+menu_setting.add_command(label="마우스 설정")
+
+menu.add_cascade(label="파일", menu=menu_file)
+menu.add_cascade(label="시작", menu=menu_start)
+menu.add_cascade(label="설정", menu=menu_setting)
+
+win.config(menu=menu)
+win.mainloop()
+
+
+
 # 반복 체크박스 (ture인 상태에서 시작을 누르면 listbox에 있는 동작들이 반복된다)
 chkvar = BooleanVar()
 checkBox = Checkbutton(win, text="반복", variable=chkvar)
 checkBox.pack();
 
-labelTitle = Label(win,text="매크로")
-labelTitle.pack()
+# 라디오 버튼
+burger_var = IntVar() # 추가(0) or 삽입(1)
+btn_burger1 = Radiobutton(win, text="추가", value=0, variable=burger_var)
+btn_burger2 = Radiobutton(win, text="삽입", value=1, variable=burger_var)
+
+btn_burger1.pack()
+btn_burger2.pack()
 
 
 listbox = Listbox(win, selectmode ="extended",height=0)
