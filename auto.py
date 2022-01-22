b72f95e43039543aa499da879a1fe15679725c62
hyjoong <maplesosser@naver.com>
2022-01-22 15:42:00 +0900

    feat: 프레임 버튼 클릭시 Toplevel생성 및 레이아웃 구현

--- auto.py
+++ auto.py
@@ -2,6 +2,7 @@ from tkinter import *
 import pyautogui
 
 # settings
+from function import get_mouse_position
 
 win = Tk()
 win.geometry("600x600")
@@ -9,10 +10,48 @@ win.title("Macro")
 win.option_add("*Font","맑은고딕 25")
 
 
+
 # variable & function
-def createNewWindow():
+def createTimeWindow():
     global new
     new = Toplevel()
+
+    def time_value():
+        temp = ent2.get()
+        listbox.insert(END,temp)
+
+    lb = Label(new, text="시간(초)를 입력하세요")
+    lb2 = Label(new, text="범위: 0.01 ~ 100초" )
+    ent2 = Entry(new,width=10)
+    success_btn = Button(new, text="완료", width=5)
+    success_btn.config(command=time_value)
+    lb.pack()
+    lb2.pack()
+    ent2.pack()
+    success_btn.pack()
+
+def createKeyWindow():
+    global new
+    new = Toplevel()
+
+    def key_value():
+        temp = ent2.get()
+        listbox.insert(END,temp)
+
+    lb = Label(new, text="추가할 키를 입력하세요")
+    ent2 = Entry(new,width=7)
+    lb.pack()
+    success_btn = Button(new, text="완료", width=5)
+    success_btn.config(command=key_value)
+    ent2.pack()
+    success_btn.pack()
+
+def createMouseWindow():
+    global new
+    new = Toplevel()
+    position = get_mouse_position();
+    lb = Label(new, text=position)
+    lb.pack()
 
 def find_location():
     place = pyautogui.position()
@@ -56,12 +95,17 @@ win.config(menu=menu)
 frame_menu = Frame(win,relief="solid", bd=1)
 frame_menu.pack(side="right", fill="both")
 
-Button(frame_menu, text="키보드").pack()
-Button(frame_menu, text="마우스").pack()
-Button(frame_menu, text="시간").pack()
-Button(frame_menu, text="지우기").pack()
 
-
+keyboard_frame_btn = Button(frame_menu, text="키보드")
+keyboard_frame_btn.config(command = createKeyWindow)
+keyboard_frame_btn.pack()
+mouse_frame_btn = Button(frame_menu, text="마우스")
+mouse_frame_btn.config(command = createMouseWindow)
+mouse_frame_btn.pack()
+time_frame_btn = Button(frame_menu, text="시간")
+time_frame_btn.config(command = createTimeWindow)
+time_frame_btn.pack()
+Button(frame_menu, text="지우기",command=delete_list).pack()
 
 
 # 반복 체크박스 (ture인 상태에서 시작을 누르면 listbox에 있는 동작들이 반복된다)
@@ -92,26 +136,9 @@ def add_list():
     print(ent.get())
     listbox.insert(END,temp)
 
-deleteBtn = Button(win, text="삭제", command=delete_list)
-deleteBtn.pack()
-
-
-btn = Button(win, text="좌표 버튼")
-btn.config(command = find_location)
-btn.pack()
-
-
-btn2 = Button(win, text="추가")
-btn2.config(command = add_list)
-btn2.pack()
-
-
-  # win.Tk()를 사용해서 새로운 Tkinter 창 생성
-
-# 클릭 시 키보드 키 입력하는 창 생성
-keyboardBtn = Button(win, text="키보드")
-keyboardBtn.config(command = createNewWindow)
-keyboardBtn.pack()
+def delete_list():
+    select = listbox.curselection()
+    print(select)
 
 win.mainloop()
 
